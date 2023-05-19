3a88f88089c23342ec740b6d88caf45666df1e25
Austin Barwick <austin.barwick@hey.com>
2023-05-19 15:02:01 -0700

    implement tic-tac-toe game

---
+++ docs/tic-tac-toe.md
@@ -0,0 +1,41 @@
+# System Design for Tic-Tac-Toe
+
+## Requirements
+
+- 2 players
+- 3x3 grid
+- 3 in a row wins
+- 9 moves max
+
+## Design
+
+### Components
+
+- Game
+- Board
+- Player
+- Move
+
+### Data Structures
+
+- Board
+  - 3x3 grid
+- Player
+    - Name
+    - Symbol
+- Move
+    - Player
+    - Cell
+
+## UX Flow
+
+1. Game intro
+2. Display board with selections
+3. Player 1 inputs number of selection in the console, hits enter
+4. Game checks the validity of the move
+5. If invalid, show error to player and loop back to 3.
+6. If valid, write symbol to cell
+7. Validate game state
+8. If Win, show win text with current player
+9. If Draw, show draw text
+10. If InProgress, restart at 3. with Player 2
---
+++ src/main.rs
@@ -0,0 +1,223 @@
+use handlebars::Handlebars;
+use std::{collections::HashMap, io};
+
+fn main() {
+    println!("Welcome to Tic-Tac-Toe!");
+    let player_one = Player {
+        symbol: PlayerSymbol::X,
+    };
+    let player_two = Player {
+        symbol: PlayerSymbol::O,
+    };
+    let mut session = GameSession::new(&player_one, &player_two);
+    session.start();
+}
+
+struct GameSession<'a> {
+    game: Game<'a>,
+}
+
+impl<'a> GameSession<'a> {
+    fn new(player_one: &'a Player, player_two: &'a Player) -> GameSession<'a> {
+        GameSession {
+            game: Game::new(player_one, player_two),
+        }
+    }
+
+    fn start(&mut self) {
+        loop {
+            let selected_cell = self.game.get_player_selection();
+            self.game.set_move(selected_cell);
+            self.game.draw_board();
+            match self.game.get_state() {
+                GameState::InProgress => {
+                    if self.game.current_player.symbol == self.game.player_one.symbol {
+                        self.game.set_current_player(self.game.player_two);
+                    } else {
+                        self.game.set_current_player(self.game.player_one);
+                    }
+                }
+                GameState::Win => {
+                    println!("Player {:?} wins!", self.game.current_player.symbol);
+                    break;
+                }
+                GameState::Draw => {
+                    println!("Draw!");
+                    break;
+                }
+            }
+        }
+    }
+}
+
+struct Game<'a> {
+    board: Board,
+    player_one: &'a Player,
+    player_two: &'a Player,
+    current_player: &'a Player,
+}
+
+impl<'a> Game<'a> {
+    fn new(player_one: &'a Player, player_two: &'a Player) -> Game<'a> {
+        Game {
+            board: Board::new(),
+            player_one,
+            player_two,
+            current_player: player_one,
+        }
+    }
+
+    /// Draws the current state of the game board
+    fn draw_board(&self) {
+        let mut hb = Handlebars::new();
+        let source = "
+            -------------
+            | {{0}} | {{1}} | {{2}} |
+            |---|---|---|
+            | {{3}} | {{4}} | {{5}} |
+            |---|---|---|
+            | {{6}} | {{7}} | {{8}} |
+            -------------
+        ";
+        hb.register_template_string("board", source).unwrap();
+        let mut hm = HashMap::new();
+        for (i, cell) in self.board.cells.iter().enumerate() {
+            hm.insert(
+                i,
+                match cell.value {
+                    Some(PlayerSymbol::X) => "X",
+                    Some(PlayerSymbol::O) => "O",
+                    None => " ",
+                },
+            );
+        }
+        println!("{}", hb.render("board", &hm).unwrap());
+    }
+
+    /// Draws the current board with numbers on the cells
+    /// that the current player can select from
+    fn draw_board_with_selections(&self) {
+        let mut hb = Handlebars::new();
+        let source = "
+            -------------
+            | {{0}} | {{1}} | {{2}} |
+            |---|---|---|
+            | {{3}} | {{4}} | {{5}} |
+            |---|---|---|
+            | {{6}} | {{7}} | {{8}} |
+            -------------
+        ";
+        hb.register_template_string("board", source).unwrap();
+        let mut hm = HashMap::new();
+        for (i, cell) in self.board.cells.iter().enumerate() {
+            hm.insert(
+                i,
+                match cell.value {
+                    Some(PlayerSymbol::X) => "X".to_string(),
+                    Some(PlayerSymbol::O) => "O".to_string(),
+                    None => i.to_string(),
+                },
+            );
+        }
+        println!("{}", hb.render("board", &hm).unwrap());
+    }
+
+    fn get_state(&self) -> GameState {
+        let winning_indices = [
+            // Horizontal
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            // Vertical
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            // Diagonal
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        for indices in winning_indices.iter() {
+            let mut winning = true;
+            for index in indices.iter() {
+                if self.board.cells[*index].value != Some(self.current_player.symbol) {
+                    winning = false;
+                    break;
+                }
+            }
+            if winning {
+                return GameState::Win;
+            }
+        }
+        let mut draw = true;
+        for cell in self.board.cells.iter() {
+            if cell.value == None {
+                draw = false;
+                break;
+            }
+        }
+        if draw {
+            return GameState::Draw;
+        }
+        GameState::InProgress
+    }
+
+    fn get_player_selection(&self) -> i8 {
+        println!("Type the number from one of the available cells:");
+        self.draw_board_with_selections();
+        let selected_index = loop {
+            let mut user_input = String::new();
+            if let Err(_) = io::stdin().read_line(&mut user_input) {
+                println!("No input, don't be typin nothin!");
+            }
+            match user_input.trim().parse::<i8>() {
+                Ok(result) if result >= 0 && result < 9 => break result,
+                Ok(_) => println!("Pick an available number from the board:"),
+                Err(_) => println!("Invalid input, pick a number from the board:"),
+            };
+        };
+        println!("Current player selected {:?}", selected_index);
+        selected_index
+    }
+
+    fn set_current_player(&mut self, player: &'a Player) {
+        self.current_player = player;
+    }
+
+    fn set_move(&mut self, cell_index: i8) {
+        self.board.cells[cell_index as usize].value = Some(self.current_player.symbol);
+    }
+}
+
+struct Board {
+    cells: Vec<Box<Cell>>,
+}
+
+impl Board {
+    fn new() -> Board {
+        let mut cells: Vec<Box<Cell>> = Vec::with_capacity(9);
+        for _ in 0..9 {
+            cells.push(Box::new(Cell { value: None }))
+        }
+        Board { cells }
+    }
+}
+
+struct Cell {
+    value: Option<PlayerSymbol>,
+}
+
+struct Player {
+    symbol: PlayerSymbol,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum PlayerSymbol {
+    X,
+    O,
+}
+
+enum GameState {
+    InProgress,
+    Win,
+    Draw,
+}
